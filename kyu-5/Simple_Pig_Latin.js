b4c8c8d228b982bd6b2257c8cbe62f7916d4a3b2
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2025-06-02 12:37:07 -0400

    working and pass initial tests

---
+++ kyu-5/Simple_Pig_Latin.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+function pigIt(str){
+  // puedo tener mas de una palabra:
+  const array = str.split(' ')
+  // * Podemos limpiar porque podemos teer muchos casos 
+  const firstWords = array.map(el => el.slice(1) + el.charAt(0) + 'ay');
+  return firstWords.join(' ');
+}
+
+console.log(pigIt('Pig latin is cool'));
+console.log(pigIt('This is my string'));
